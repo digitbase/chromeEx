e1f79f816a5252e48897219da8d0f0bc5a55bf84
kumanduo <kumanduo@qq.com>
2020-07-04 12:51:54 +0800

    commmit

--- content-script.js
+++ content-script.js
@@ -1,10 +1,6 @@
-﻿console.log(" content-script this run ");
-
-window.addEventListener("mouseup", wordSelected);
-
+﻿window.addEventListener("mouseup", wordSelected);
 function wordSelected(evet) {
-	let selectText = window.getSelection().toString();
-	console.log(selectText);
+	let selectText = window.getSelection().toString().trim();
 	if (selectText.length > 0) {
 		chrome.runtime.sendMessage(selectText);
 	}
